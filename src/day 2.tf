6565138e493364a975adecd0eb74fe6d97c15a09
rauf <rauf.64276@gmail.com>
2026-09-18 15:01:12 -0400

    Clean Terraform project

---
+++ src/day 2.tf
@@ -0,0 +1,38 @@
+/*terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+/*provider "aws" {
+  region = "us-east-1"
+}*/
+#AWS provider
+/*provider "aws" {
+  region     = "us-west-2"
+
+}*/
+
+
+
+/*resource "aws_instance" "ec2-instanceRauf" {
+
+  ami           = "ami-0bea529386a62a2ad"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "EC2_instance_RaufVafa"
+    Owner =  "RAUF BAIRAMOV"
+    Project = "Terraform"
+  }
+}
+
+
+# # Create a VPC
+# resource "aws_vpc" "example" {
+#   cidr_block = "10.0.0.0/16"
+# }*/
