1e75b27093d79ddf433cfa856d502a54f106273f
rauf <rauf.64276@gmail.com>
2026-09-18 22:39:44 -0400

    updates some comments

---
+++ src/Lesson_5/WebServerDynamicSecurityGroup.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+
+#AWS provider you have to go IAM
+provider "aws" {
+  region     = "us-west-2"
+
+
+}
+
+
+
+
+resource "aws_security_group" "my_webserver" {
+  name        = "WebServer Security Group"
+  description = "Allow HTTPS inbound traffic and all outbound traffic"
+
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+
+# -------------------------
+# HTTPS - IPv4
+# -------------------------
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.my_webserver.id
+  cidr_ipv4         = "0.0.0.0/0"
+
+  from_port   = 80
+  to_port     = 80
+  ip_protocol = "tcp"
+}
+
+
+# -------------------------
+# HTTPS - IPv6
+# -------------------------
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
+  security_group_id = aws_security_group.my_webserver.id
+  cidr_ipv6         = "::/0"
+
+  from_port   = 80
+  to_port     = 80
+  ip_protocol = "tcp"
+}
+
+
+# -------------------------
+# Allow all outbound IPv4
+# -------------------------
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.my_webserver.id
+  cidr_ipv4         = "0.0.0.0/0"
+
+  ip_protocol = "-1"
+}
+
+
+# -------------------------
+# Allow all outbound IPv6
+# -------------------------
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.my_webserver.id
+  cidr_ipv6         = "::/0"
+
+  ip_protocol = "-1"
+}
