16594b5c3a916ac64c6651b545abccdb80b1f4b0
rauf <rauf.64276@gmail.com>
2026-09-19 00:32:24 -0400

    updates some comments

--- src/Lesson_5/WebServerDynamicSecurityGroup.tf
+++ src/Lesson_5/WebServerDynamicSecurityGroup.tf
@@ -7,71 +7,47 @@ terraform {
   }
 }
 
-
-#AWS provider you have to go IAM
+# AWS Provider
 provider "aws" {
-  region     = "us-west-2"
-
-
+  region = "us-west-2"
 }
 
-
-
-
+# Security Group
 resource "aws_security_group" "my_webserver" {
-  name        = "WebServer Security Group"
-  description = "Allow HTTPS inbound traffic and all outbound traffic"
+  name        = "Dynamic Security Group"
+  description = "Security Group with dynamic inbound rules"
 
+  # Dynamic inbound rules
+  dynamic "ingress" {
+    for_each = [80, 443, 8080, 1541, 9092]
+
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.10.0.0/16"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name = "allow_tls"
+    Name    = "Dynamic-Security-Group"
+    Project = "Terraform"
+    Owner   = "Rauf Bairamov"
   }
-}
-
-
-# -------------------------
-# HTTPS - IPv4
-# -------------------------
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.my_webserver.id
-  cidr_ipv4         = "0.0.0.0/0"
-
-  from_port   = 80
-  to_port     = 80
-  ip_protocol = "tcp"
-}
-
-
-# -------------------------
-# HTTPS - IPv6
-# -------------------------
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-  security_group_id = aws_security_group.my_webserver.id
-  cidr_ipv6         = "::/0"
-
-  from_port   = 80
-  to_port     = 80
-  ip_protocol = "tcp"
-}
-
-
-# -------------------------
-# Allow all outbound IPv4
-# -------------------------
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.my_webserver.id
-  cidr_ipv4         = "0.0.0.0/0"
-
-  ip_protocol = "-1"
-}
-
-
-# -------------------------
-# Allow all outbound IPv6
-# -------------------------
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.my_webserver.id
-  cidr_ipv6         = "::/0"
-
-  ip_protocol = "-1"
 }
